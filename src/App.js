fbd173470227f53e4307a2293e8b949d8bb41adf
İlkay Aktaş <ilkayaktas@gmail.com>
2022-03-19 23:27:01 +0300

    ExpenseDate component is created. Expenses component is created.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Expenses from './components/Expenses'
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
@@ -23,15 +24,10 @@ function App() {
         },
     ];
 
-
-
   return (
       <div>
         <h2>Let's get started!</h2>
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+        <Expenses items={expenses}/>
       </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
+
+
     return (
         <div className="expense-item">
-            <div>{props.date.toUTCString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
