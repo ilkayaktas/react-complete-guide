da035097d32a4af79434d1f3819fa3b280b41465
İlkay Aktaş <ilkayaktas@gmail.com>
2022-03-20 15:27:56 +0300

    useState hook is used ot handle button click action.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from './ExpenseDate'
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-    const clickedHandler = () => {
-        console.log('Clicked!');
-    }
+    const [title, setTitle] = useState(props.title)
 
+    const clickedHandler = () => {
+        setTitle('Updated!')
+        console.log(title);
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
             <button onClick={clickedHandler}>Change Title</button>
