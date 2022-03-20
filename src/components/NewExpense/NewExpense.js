71e975083732b70291ebedc1f02afbe8fc178cfe
İlkay Aktaş <ilkayaktas@gmail.com>
2022-03-20 22:16:13 +0300

    NewExpense form is added. User may enter new values from input form.

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './NewExpense.css'
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(){
+    return <div className='new-expense'>
+        <ExpenseForm/>
+    </div>
+}
+
+export default NewExpense;
