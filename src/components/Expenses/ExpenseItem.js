19f6ca5d3195c4813b3f906c8fe827171afb157b
İlkay Aktaş <ilkayaktas@gmail.com>
2022-03-20 15:10:28 +0300

    Temporary button is added.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,6 +13,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
+            <button onClick={() => {console.log('Clicked!')}}>Change Title</button>
         </Card>
     )
 }
