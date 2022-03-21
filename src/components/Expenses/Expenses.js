e154683d0746a92c51210c469c82b8ad9804a710
İlkay Aktaş <ilkayaktas@gmail.com>
2022-03-21 16:19:52 +0300

    Item array is handled and rendered in as list.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,10 +14,9 @@ function Expenses(props){
         <div>
             <Card className='expenses'>
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
-                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
-                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
-                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
-                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/>
+                {props.items.map((expense) => (
+                    <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>
+                ))}
             </Card>
         </div>
 
